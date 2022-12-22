286bbede05f852bf07ff3cc8c395f1427d21a15e
Jürg Staub <juergstaub@users.noreply.github.com>
2022-12-22 11:18:01 +0100

    Update train.py
    added split_data function

--- src/model/train.py
+++ src/model/train.py
@@ -3,6 +3,8 @@
 import argparse
 import glob
 import os
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 import pandas as pd
 
@@ -33,7 +35,9 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
+def split_data(df):
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    return train_test_split(X, y, test_size=0.30, random_state=0) 
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
